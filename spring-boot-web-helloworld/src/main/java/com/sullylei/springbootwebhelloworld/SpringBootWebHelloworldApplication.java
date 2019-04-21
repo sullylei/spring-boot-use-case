9250e949ee6938d990c0bacdc5617670d410d504
sullylei <sullylei0207@gmail.com>
2019-04-21 23:41:14 +0800

    新建springboot的用例项目

---
+++ spring-boot-web-helloworld/src/main/java/com/sullylei/springbootwebhelloworld/SpringBootWebHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.sullylei.springbootwebhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebHelloworldApplication.class, args);
+    }
+
+}
